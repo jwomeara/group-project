bdb28418a24bf15b429ec60287c5d168f58b27f7
stephanephanon <stephanephanon@users.noreply.github.com>
2014-05-05 14:06:59 -0400

    Update PlaneTracker.cs
    Trying to put bounds on the tracking function.  This version finds the closest neighbor polygon in frame ti and checks if its centroid is within the plane under consideration's polygon.  If it is, we consider it a match.  If not, we hold onto the plane at its current position and start a counter to kill the plane.  Slightly more robust than a global cut-off for distance, but this still might not be the best way.

--- code_c#/CS682Project/PlaneTracker.cs
+++ code_c#/CS682Project/PlaneTracker.cs
@@ -15,17 +15,14 @@ namespace CS682Project
 {
     class PlaneTracker
     {
-
         private List<Plane> planes;
 
         // assume the user has associated images with identified planes in the gui. we send these to the tracker constructor.
-        public PlaneTracker(List<Plane> planes)
-        {
+        public PlaneTracker(List<Plane> planes) {
             this.planes = planes;
         }
 
-        public List<Plane> GetPlanes()
-        {
+        public List<Plane> GetPlanes() {
             return this.planes;
         }
 
@@ -34,64 +31,141 @@ namespace CS682Project
         ///  no return type. implicitly updates planes given to it.
         /// </summary>
         /// <param name="polys">polygons detected in the current frame</param>
-        public void UpdatePlanes(List<System.Drawing.Point[]> polys)
-        {
-            // compare each plane being tracked to the current detected polys
+        public void UpdatePlanes(List<System.Drawing.Point[]> polys) {
+           // track best matches as we go through as <polyIndex, List<planeIndex, distance>>
+            Dictionary<int, List<Tuple<int, double>>> matches = new Dictionary<int, List<Tuple<int, double>>>();
+
+            List<Plane> trackedPlanes = new List<Plane>();
+
+            // if we don't find a match after this many frames, remove the plane
+            int deathCount = 30;
+
+            // calculate all the centroids for the polygons in the current frame
+            List<System.Drawing.PointF> polyCentroids = new List<System.Drawing.PointF>();
+            
+            for (int i = 0; i < polys.Count; i++)
+            {
+                System.Drawing.PointF polyCentroid = new System.Drawing.Point();
+                polyCentroid = calculateCentroid(polys.ElementAt(i));
+                polyCentroids.Add(polyCentroid);
+            }
+
+            // check if the plane has reached its limit of not having a matching polygon. if so, remove it
+            foreach (Plane plane in this.planes)
+            {
+                if (plane.GetDeathClock() < deathCount)
+                {
+                    trackedPlanes.Add(plane);
+                }
+            }
+
+            this.planes = trackedPlanes;
+
+            // compare each plane being tracked to each of the polygons in the current frame
             for (int i = 0; i < this.planes.Count; i++)
             {
                 double bestDistance = -1;
                 int bestIndex = -1;
+                System.Drawing.PointF bestCentroid;
 
                 // 1. calculate centroid of the plane
                 System.Drawing.PointF planeCentroid = calculateCentroid(this.planes.ElementAt(i).GetPoints());
 
-                System.Diagnostics.Debug.WriteLine("plane index: " + i);
-                System.Diagnostics.Debug.WriteLine("plane centroid: " + planeCentroid.X + " " + planeCentroid.Y);
-        
+                // compare to each of the polygons in the current frame
                 for (int j = 0; j < polys.Count; j++)
                 {
-                    // 1.calculate centroid of the poly
-                    System.Drawing.PointF polyCentroid = new System.Drawing.Point();
-                    polyCentroid = calculateCentroid(polys.ElementAt(j));
-
-                    System.Diagnostics.Debug.WriteLine("poly index: " + j);
-                    System.Diagnostics.Debug.WriteLine("poly centroid: " + polyCentroid.X + " " + polyCentroid.Y);
-        
-
                     //2. calculate euclidean distance between poly and plane centroids
                     double distance = Math.Sqrt(
-                        Math.Pow((planeCentroid.X - polyCentroid.X), 2) + 
-                        Math.Pow((planeCentroid.Y - polyCentroid.Y), 2)
+                        Math.Pow((planeCentroid.X - polyCentroids[j].X), 2) + 
+                        Math.Pow((planeCentroid.Y - polyCentroids[j].Y), 2)
                         );
 
-                    if (bestDistance == -1)
-                    {
+                    if (bestDistance == -1) {
                         bestDistance = distance;
                         bestIndex = j;
+                        bestCentroid = polyCentroids[j];
                     }
-                    else
-                    {
+                    else {
                         if (distance < bestDistance)
                         {
                             bestDistance = distance;
                             bestIndex = j;
+                            bestCentroid = polyCentroids[j];
                         }
                     }
+                } // end of polys loop
+
+                 // when a potential plane/poly match is made, we track it. bestPolyIndex, List[(planeIndex, bestDistance)]
+                if (!matches.ContainsKey(bestIndex)) {
+                    // add the bestIndex key to matches
+                    matches.Add(bestIndex, new List<Tuple<int, double>>());
                 }
+                matches[bestIndex].Add(Tuple.Create<int, double>(i, bestDistance));
+            } // end of planes for loop
 
-            // when the id is made, update the points in the corresponding plane that is being tracked using bestDistance and bestIndex
-            // first check if bestDistance is less than threshold (we'll say less than 50 pix for now)
-            // TODO -- SNS to work on the threshold.
-                double thresholdDistance = 50;
+            // now we need to check the dictionary to find if any polys get associated with multiple planes
+            foreach (KeyValuePair<int, List<Tuple<int, double>>> entry in matches)
+            {
+                int polyIndex = entry.Key;
+                List<Tuple<int, double>> candidates = entry.Value;
 
-                System.Diagnostics.Debug.WriteLine("best distance: " + bestDistance);
-
-                if (bestDistance < thresholdDistance)
-                {
-                    System.Diagnostics.Debug.WriteLine("i and bestIndex: " + i + " " + bestIndex);
-                    this.planes.ElementAt(i).SetPoints(polys.ElementAt(bestIndex));        
+                // case 1. only one plane matched this polygon
+                if (candidates.Count == 1) {
+                 
+                    // check if the polygon's centroid is inside or incident to the plane. if it is consider it a match
+                    if (pointPolygonTest(this.planes.ElementAt(candidates[0].Item1).GetPoints(), polyCentroids[polyIndex]) > -1)
+                    {
+                        this.planes.ElementAt(candidates[0].Item1).SetPoints(polys.ElementAt(polyIndex));
+                        this.planes.ElementAt(candidates[0].Item1).SetDeathClock(0);
+                    }
+                    // otherwise we won't use as a match. increment the death clock and keep current set of points for this plane.
+                    else
+                    {
+                        this.planes.ElementAt(candidates[0].Item1).SetDeathClock(this.planes.ElementAt(candidates[0].Item1).GetDeathClock() + 1);
+                    }      
                 }
-            }
+                // case 2. multiple things in the list. find smallest distance match.
+                else {
+                    int smallestDistanceIndex = 0;
+
+                    // find the smallest distance of the candidates
+                    for (int i = 0; i < candidates.Count; i++)
+                    {
+                        double smallestDistance = candidates[smallestDistanceIndex].Item2;
+                        double currentDistance = candidates[i].Item2;
+
+                        if (currentDistance <= smallestDistance) {
+                            smallestDistanceIndex = i;
+                        }
+                    }
+
+                    // given the smallest distance, update the plane that it goes with and increment deathclock for the rest of planes
+                    for (int i = 0; i < candidates.Count; i++)
+                    {
+                        int planeIndex = candidates[i].Item1;
+
+                        // set the smallest distance polygon
+                        if (i == smallestDistanceIndex) {
+
+                            // check if the polygon's centroid is inside or incident to the plane. if it is consider it a match
+                            if (pointPolygonTest(this.planes.ElementAt(planeIndex).GetPoints(), polyCentroids[polyIndex]) > -1)
+                            {
+                                this.planes.ElementAt(planeIndex).SetPoints(polys.ElementAt(polyIndex));
+                                this.planes.ElementAt(planeIndex).SetDeathClock(0);
+                            }
+                            // otherwise we won't use as a match. increment the death clock and keep current set of points for this plane.
+                            else
+                            {
+                                this.planes.ElementAt(candidates[0].Item1).SetDeathClock(this.planes.ElementAt(candidates[0].Item1).GetDeathClock() + 1);
+                            }
+                        }
+                        // otherwise, there was a polygon closer than this one. increment the death clock and keep current set of points for the plane
+                        else {
+                            this.planes.ElementAt(planeIndex).SetDeathClock(this.planes.ElementAt(planeIndex).GetDeathClock() + 1);
+                        }
+                    }
+                } // end of multiple matches else
+            } // end of dictionary check
         }
 
         /// <summary>
@@ -117,5 +191,59 @@ namespace CS682Project
 
             return new System.Drawing.PointF(cX, cY);
         }
+
+        /// <summary>
+        /// Takes the polygon vertices and the point.  Determines where the point lies.
+        /// This is a direct port of the opencv code on github b/c i couldn't get the cvInvoke version working
+        /// </summary>
+        /// <param name="polyCoords">vertices of the polygon to use</param>
+        /// <param name="point">point to check on</param>
+        /// <returns>-1 if not in or on, 0 if on, 1 if in</returns>
+         private int pointPolygonTest(System.Drawing.Point[] polygonVertices, System.Drawing.PointF point)
+         {
+             int result = 0;
+             int counter = 0;
+         
+             System.Drawing.PointF v0, v;
+         
+             v = polygonVertices[polygonVertices.Count() - 1];
+
+             for (int i = 0; i < polygonVertices.Count(); i++)
+             {
+                double dist;
+                v0 = v;
+                v = polygonVertices[i];
+
+                if((v0.Y <= point.Y && v.Y <= point.Y) ||
+                   (v0.Y > point.Y && v.Y > point.Y) ||
+                   (v0.X < point.X && v.X < point.X) )
+                {
+                    if (point.Y == v.Y && (point.X == v.X || (point.Y == v0.Y &&
+                        ((v0.X <= point.X && point.X <= v.X) || (v.X <= point.X && point.X <= v0.X)))))
+                    {
+                        return 0;
+                    }
+                    continue;
+                }
+
+                dist = (double)(point.Y - v0.Y)*(v.X - v0.X) - (double)(point.X - v0.X)*(v.Y - v0.Y);
+               
+                if( dist == 0 ) {
+                    return 0;
+                }
+                
+                if( v.Y < v0.Y) {
+                    dist = -dist;
+                }
+
+                // counter += dist > 0;
+                 if (dist > 0) {
+                     counter = counter + 1;
+                 }
+             }
+             result = (counter % 2 == 0) ? -1 : 1;
+
+             return result;
+         }
     }
 }
